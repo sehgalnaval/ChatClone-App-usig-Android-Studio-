e0c5dfc92e542a0ecb5e01d1d2272131401441ba
Naval sehgal <88925485+sehgalnaval@users.noreply.github.com>
2024-06-14 19:01:00 +0530

    data

---
+++ DataTypes.kt
@@ -0,0 +1,45 @@
+package com.example.chatappclone.data
+
+data class UserData(
+    val userId: String? = "",
+    val name: String? = "",
+    val number: String? = "",
+    val imageUrl: String? = ""
+) {
+    fun toMap() = mapOf(
+        "userId" to userId,
+        "name" to name,
+        "number" to number,
+        "imageUrl" to imageUrl
+    )
+}
+
+data class ChatData(
+    val chatId: String? = "",
+    val user1: ChatUser = ChatUser(),
+    val user2: ChatUser = ChatUser()
+)
+
+data class ChatUser(
+    val userId: String? = "",
+    val name: String? = "",
+    val imageUrl: String? = "",
+    val number: String? = ""
+)
+
+data class Message(
+    val sentBy: String? = "",
+    val message: String? = "",
+    val timestamp: String? = ""
+)
+
+data class Status(
+    val user: ChatUser = ChatUser(),
+    val imageUrl: String? = "",
+    val timestamp: Long? = null
+)
+
+
+
+
+
